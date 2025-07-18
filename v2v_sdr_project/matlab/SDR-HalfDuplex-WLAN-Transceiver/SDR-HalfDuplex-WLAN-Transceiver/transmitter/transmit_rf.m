7c6527e7d79b5e3819a15b41b04bb64e2f1186e3
pollyjuice74 <hernandez.aht82836@gmail.com>
2025-07-18 14:59:00 -0400

    Connecting SDR with ROS container via ip

--- v2v_sdr_project/matlab/SDR-HalfDuplex-WLAN-Transceiver/SDR-HalfDuplex-WLAN-Transceiver/transmitter/transmit_rf.m
+++
@@ -1,9 +0,0 @@
-function transmit_rf(sdrTransmitter, txWaveform)
-    fprintf('\nTransmitting WLAN waveform...\n');
-
-    powerScaleFactor = 0.8;
-    txWaveform = txWaveform .* (1/max(abs(txWaveform)) * powerScaleFactor);
-
-    fprintf('[TX] Transmitting waveform...\n');
-    transmitRepeat(sdrTransmitter, txWaveform);
-end
